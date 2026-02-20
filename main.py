d4bd6d37e8cd3b37847d32324281ee11babb595f
MaxwellML <cmdp7@outlook.com>
2026-02-20 17:21:04 +0000

    Added window again but fixed issue with fully yellow background displaying.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 import rasterio
-from pyproj import Transformer
-from rasterio.windows import from_bounds
 from rasterio.plot import show
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from pyproj import Transformer
 
 tif_path = Path(__file__).resolve().parent / "SZ49se_FZ_DSM_1m.tif"
 
@@ -12,29 +11,33 @@ lon = float(input("Enter Longitude (e.g. -1.3276): "))
 lat = float(input("Enter Latitude  (e.g. 50.730251): "))
 
 with rasterio.open(tif_path) as src:
+
     t = Transformer.from_crs("EPSG:4326", src.crs, always_xy=True)
     E, N = t.transform(lon, lat)
 
-    L1 = 1000
-    h1 = L1 / 2
-    left1, right1 = E - h1, E + h1
-    bot1, top1     = N - h1, N + h1
-
-    win = from_bounds(left1, bot1, right1, top1, transform=src.transform)
-    arr = src.read(1, window=win)
-    win_transform = src.window_transform(win)
-
     fig, ax = plt.subplots()
-    show(arr, transform=win_transform, ax=ax)
+    show(src, ax=ax)  
 
-    ax.add_patch(Rectangle((left1, bot1), L1, L1, facecolor="none", linewidth=2))
+    L_square = 100
+    h_square = L_square / 2
+    ax.add_patch(
+        Rectangle(
+            (E - h_square, N - h_square), 
+            L_square, L_square,
+            fill=True,     
+            linewidth=2,
+            zorder=10
+        )
+    )
 
-    L2 = 100
-    h2 = L2 / 2
-    left2, bot2 = E - h2, N - h2
-    ax.add_patch(Rectangle((left2, bot2), L2, L2, fill=True, alpha=0.3, linewidth=2))
+    L_region = 1000
+    h_region = L_region / 2
 
-    ax.set_xlim(left1, right1)
-    ax.set_ylim(bot1, top1)
+    left, right = E - h_region, E + h_region
+    bottom, top = N - h_region, N + h_region
 
+    ax.add_patch(Rectangle((left, bottom), L_region, L_region, fill=False, linewidth=2, zorder=10))
+
+    ax.set_xlim(left, right)
+    ax.set_ylim(bottom, top)
     plt.show()
